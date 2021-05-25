175a33ccbece3acc0d6a068ede6ad11ca3fa7198
DhimitriosDuka <dhimitrios.duka@fti.edu.al>
2021-05-25 12:27:50 +0200

    Implemented H2 database.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE ingredient(
 	id SERIAL PRIMARY KEY,
-	name CHARACTER VARYING(50) NOT NULL,
+	name CHARACTER VARYING(50) NOT NULL UNIQUE ,
 	calories NUMERIC NOT NULL,
 	amount NUMERIC NOT NULL,
 	unit CHARACTER VARYING(10),
@@ -14,28 +14,30 @@ CREATE TABLE ingredient(
 
 CREATE TABLE menu_item (
 	id SERIAL PRIMARY KEY,
-	name CHARACTER VARYING(50) NOT NULL,
+	name CHARACTER VARYING(50) NOT NULL UNIQUE ,
 	price NUMERIC NOT NULL,
 	michelin_stars INT,
 	course CHARACTER VARYING(20),
 	description CHARACTER VARYING(50),
 	type CHARACTER VARYING(20),
-	category CHARACTER VARYING(20)
+	category CHARACTER VARYING(20),
+	currency CHARACTER VARYING(20) NOT NULL ,
+	calories NUMERIC NOT NULL
 );
 
 CREATE TABLE menu_item_ingredient (
 	amount INT NOT NULL,
 	ingredient_id BIGINT REFERENCES ingredient(id),
-	dish_id BIGINT REFERENCES menu_item(id),
-	PRIMARY KEY(ingredient_id, dish_id)
+	menu_item_id BIGINT REFERENCES menu_item(id),
+	PRIMARY KEY(ingredient_id, menu_item_id)
 );
 
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
 	first_name CHARACTER VARYING(20) NOT NULL,
 	last_name CHARACTER VARYING(20) NOT NULL,
-	user_name CHARACTER VARYING(20) NOT NULL,
-	email CHARACTER VARYING(40) NOT NULL,
+	user_name CHARACTER VARYING(20) NOT NULL UNIQUE ,
+	email CHARACTER VARYING(40) NOT NULL UNIQUE,
 	password CHARACTER VARYING(255) NOT NULL,
 	telephone_number CHARACTER VARYING(20),
 	address CHARACTER VARYING(50),
@@ -52,14 +54,17 @@ CREATE TABLE orders (
 	created_at DATE NOT NULL,
 	delivery_time DATE NOT NULL,
 	costumer_id BIGINT REFERENCES users(id),
-	delivery_guy_id BIGINT REFERENCES users(id)
+	delivery_guy_id BIGINT REFERENCES users(id),
+	phone_number CHARACTER VARYING(20) NOT NULL,
+	address CHARACTER VARYING(20) NOT NULL
 );
 
 CREATE TABLE orders_menu_items (
 	amount INT NOT NULL,
 	menu_item_id BIGINT REFERENCES menu_item(id) ,
 	order_id BIGINT REFERENCES orders(id),
-	PRIMARY KEY(ingredient_id, dish_id)
+	note VARCHAR(50),
+	PRIMARY KEY(menu_item_id, order_id)
 );
 
 
