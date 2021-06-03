df7df1646e317ec3cc3cc55b4f0118e09f6dad99
DhimitriosDuka <dhimitrios.duka@fti.edu.al>
2021-06-03 12:29:09 +0200

    Updated Path with new endpoints.

--- src/main/java/com/rms/rms/utils/Path.java
+++ src/main/java/com/rms/rms/utils/Path.java
@@ -7,15 +7,22 @@ import lombok.Getter;
 @Getter
 public class Path {
 
-    public static final String INGREDIENT_PATH = "/ingredients";
     public static final String ID = "/{id}";
+    public static final String INGREDIENT_PATH = "/ingredients";
+    public static final String USER_PATH = "/users";
     public static final String MENU_ITEM_PATH = "/menuItems";
-    public static final String MENU_ID_INGREDIENT = "/{menuItemId}/ingredients/{ingredientId}";
-    public static final String SET_MENU_ITEM_UNAVAILABLE = "/{menuItemId}/unavailable";
-    public static final String SET_MENU_ITEM_AVAILABLE = "/{menuItemId}/available";
+    public static final String ORDER_PATH = "/orders";
     public static final String UPDATE_ROLE_OF_USER_WITH_ID = ID + "/role";
     public static final String UPDATE_PASSWORD_OF_USER_WITH_ID = ID + "/password";
+    public static final String SCHEDULE_PATH = "/{userId}/schedule";
+    public static final String SCHEDULE_UPDATE_PATH = "/schedule/{scheduleId}";
+    public static final String MENU_ID_INGREDIENT = "/{menuItemId}/ingredients/{ingredientId}";
+    public static final String DELETE_MENU_ITEM_FROM_ORDER = "/{orderId}/menu-item/{menuItemId}";
+    public static final String ADD_MENU_ITEM_TO_ORDER = "/{id}/menu-item";
+    public static final String SET_MENU_ITEM_UNAVAILABLE = "/{menuItemId}/unavailable";
+    public static final String SET_MENU_ITEM_AVAILABLE = "/{menuItemId}/available";
+    public static final String UPDATE_STATUS_ORDER_PATH = "/{id}/status";
+    public static final String CANCEL_ORDER_PATH = ID + "/status/cancel";
 
-    public static final String USER_PATH = "/users";
 
 }
